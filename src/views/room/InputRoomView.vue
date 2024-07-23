594ad713ba60a5648432d04f19d61b557f1215e9
hairus <hairus@sumenepsmansa.sch.id>
2024-07-23 20:55:36 +0700

    sm

--- src/views/room/InputRoomView.vue
+++ src/views/room/InputRoomView.vue
@@ -5,15 +5,7 @@
       <div class="text-end justify-end me-5">
         <button type="button"
           class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 focus:outline-none focus:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-400 dark:bg-blue-800/30 dark:hover:bg-blue-800/20 dark:focus:bg-blue-800/20"
-<<<<<<< Updated upstream
-          aria-haspopup="dialog"
-          aria-expanded="false"
-          aria-controls="hs-large-modal"
-          data-hs-overlay="#hs-large-modal"
-        >
-=======
           aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-large-modal" data-hs-overlay="#hs-large-modal">
->>>>>>> Stashed changes
           + Tambah Data User
         </button>
       </div>
